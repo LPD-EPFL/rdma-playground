7cbe1e7d6d642dda9ceb51a65160ba05dd062a88
Athanasios Xygkis <xygkis@lpdquatro1.epfl.ch>
2019-10-24 10:39:20 +0200

    Add deployer script

---
+++ deploy.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+nodes=("lpdquatro2" "lpdquatro3" "lpdquatro4")
+
+config_toml=$(cat <<EOF
+[registry]
+    host = \"#host#\"
+    port = 11211
+
+[general]
+    clients = #clients#
+    id = #id#
+EOF
+)
+
+startme() {
+    # Start memcached on the first node
+    ip=`getent hosts ${nodes[0]} | awk '{ print $1 }'`
+
+    for ((j=0; j<${#nodes[@]}; j++)); do
+        # if [[ "x${nodes[$j]}" == "x$HOSTNAME" ]]; then
+        #     continue
+        # fi
+
+        if [ "$j" -eq "0" ]; then
+            start_memcached='tmux new-window -n memcached -t peregrin:1; tmux send-keys -t peregrin:memcached "memcached -l 0.0.0.0" C-m; tmux select-window -t peregrin:0'
+        else
+            start_memcached=""
+        fi
+
+ssh "$USER@${nodes[$j]}" /bin/bash << EOF
+    tmux start-server
+    tmux kill-session -t peregrin 2> /dev/null
+    mkdir -p $DIR/peregrin_deployment
+    cd $DIR/peregrin_deployment
+    echo "$config_toml" > config.${nodes[$j]}.toml
+    tmux new-session -d -n main -s peregrin
+    $start_memcached
+    export PEREGRIN_NN=${nodes[$j]}
+    sed -i "s/#host#/$ip/" config.${nodes[$j]}.toml
+    sed -i "s/#clients#/${#nodes[@]}/" config.${nodes[$j]}.toml
+    sed -i "s/#id#/$j/" config.${nodes[$j]}.toml
+    tmux send-keys -t peregrin:main "CONFIG=config.${nodes[$j]}.toml $DIR/builddir/propose-test" C-m
+EOF
+
+    done
+    echo "Done"
+}
+
+stopme() {
+    for ((j=0; j<${#nodes[@]}; j++)); do
+        ssh "$USER@${nodes[$j]}" "tmux kill-session -t peregrin"
+    done
+}
+
+case "$1" in
+    start)   startme ;;
+    stop)    stopme ;;
+    *) echo "usage: $0 start|stop" >&2
+       exit 1
+       ;;
+esac
